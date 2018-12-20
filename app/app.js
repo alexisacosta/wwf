1be12cbb42e01c6187b48386d0b004c588652a21
Alexis Acosta <alexis.acosta.1984@gmail.com>
2018-12-20 19:00:15 -0300

    Actualizacion 20122018

--- app/app.js
+++ app/app.js
@@ -1,6 +1,14 @@
 'use strict'
 
-var app = angular.module("wwf",['ngRoute', 'wwfControl'])
+var app = angular.module("wwf",['ngRoute','ngMaterial','ngMessages', 'wwfControl'])
+
+app.config(function($routeProvider){
+	$routeProvider
+	.when('/',{
+		title: 'Login',
+		templateUrl: 'pages/login.html'
+	})
+})
 
 
 
